8ebab8e66866999fcaa589029251f7cd85b51748
Ahmet Kale <116072726+Vandu35@users.noreply.github.com>
2023-01-11 05:11:00 +0300

    Add files via upload

---
+++ Lab/lab 10/lab 10-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int i = 0,a[10],p;
+	
+	for(i = 0; i < 10; i++){
+		printf("\n%d . sayiyi girniz :" ,i + 1);
+		scanf("%d"  ,&a[i]);
+		if (a[i] != -1){
+			p = pow(a[i],2);
+			printf("\n%d\n" ,p);
+			
+		}
+		else{
+			a[i] = 0;
+			return 0;
+		}
+		
+	}
+
+	return 0;
+}
