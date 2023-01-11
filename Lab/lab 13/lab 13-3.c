f6e838243b265c80375efd5a9762ad5b31784690
Ahmet Kale <116072726+Vandu35@users.noreply.github.com>
2023-01-11 21:22:26 +0300

    Add files via upload

---
+++ Lab/lab 13/lab 13-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int is_even(int num) {
+    return (num % 2 == 0);
+}
+
+int main() {
+    int num;
+
+    printf("Enter an integer to check if it's even: ");
+    scanf("%d", &num);
+
+    if (is_even(num)) {
+        printf("The number %d is even.\n", num);
+    } else {
+        printf("The number %d is odd.\n", num);
+    }
+
+    return 0;
+}
+
---
+++ Lab/lab 13/lab 13-5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int wordctr(char a[100]){
+	int i,ctr=1;
+	for (i=0; a[i] != '\0'; i++){
+		if (a[i] == ' ' && a[i+1] != ' '){
+			ctr++;
+		}
+	}
+	return ctr;
+}
+
+int main (){
+	char str1[100];
+	printf("write something :");
+	gets(str1);
+		
+	printf("Number of Words : %d" ,wordctr(str1));
+	
+	return 0;
+}
